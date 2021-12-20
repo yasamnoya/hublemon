a1a1695d4ee14912eedd6c42b56c4cfb45313c6c
yasamnoya <yasamnono@gmail.com>
2021-12-20 20:20:45 +0800

    add cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const router = require('./routes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('common'));
 
