be4c9774464c42c84047c8dda2302aec8c86263f
yasamnoya <yasamnono@gmail.com>
2021-12-20 20:22:20 +0800

    add packages

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
+import axios from 'axios';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'taipei-sans-tc';
+import 'taipei-sans-tc/dist/Light/TaipeiSansTCBeta-Light.css';
+import 'taipei-sans-tc/dist/Bold/TaipeiSansTCBeta-Bold.css';
+import 'typeface-noto-sans-tc';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
 
 new Vue({
   router,
