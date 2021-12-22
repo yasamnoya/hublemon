5906e68025de6b3286be194adc6dff9c47c5f135
yasamnoya <yasamnono@gmail.com>
2021-12-23 00:07:54 +0800

    fix CommentCard RWD

--- client/src/components/CommentCard.vue
+++ client/src/components/CommentCard.vue
@@ -2,28 +2,34 @@
   <div>
     <div class="card my-2 shadow">
       <div class="card-body">
-        <div class="d-flex">
-          <img
-            width="32px"
-            height="32px"
-            :src="
-              comment.author.avatarUrl ||
-              'https://iconorbit.com/icons/256-watermark/0204201617425658313-Lemon%20Half%20Cut.jpg'
-            "
-            :alt="`Avatar of ${comment.author.name}`"
-            class="rounded-circle me-3"
-          />
-          <h5 class="me-2 fw-bold">{{ comment.author.name }}</h5>
-          <p v-if="comment.provider == 'wiwivideo'">
-            <span class="badge bg-wiwivideo rounded-pill me-2">wiwi.video</span>
-          </p>
-          <p v-if="comment.provider == 'odysee'">
-            <span class="badge bg-odysee rounded-pill me-2">Odysee</span>
-          </p>
-          <p v-if="comment.provider == 'youtube'">
-            <span class="badge bg-youtube rounded-pill me-2">Youtube</span>
-          </p>
-          <p class="text-muted">{{ comment.createdDateString }}</p>
+        <div class="d-flex flex-column flex-md-row">
+          <div class="d-flex">
+            <img
+              width="32px"
+              height="32px"
+              :src="
+                comment.author.avatarUrl ||
+                'https://iconorbit.com/icons/256-watermark/0204201617425658313-Lemon%20Half%20Cut.jpg'
+              "
+              :alt="`Avatar of ${comment.author.name}`"
+              class="rounded-circle me-3"
+            />
+            <h5 class="me-2 fw-bold">{{ comment.author.name }}</h5>
+            <div class="me-3">
+              <p v-if="comment.provider == 'wiwivideo'">
+                <span class="badge bg-wiwivideo rounded-pill">wiwi.video</span>
+              </p>
+              <p v-if="comment.provider == 'odysee'">
+                <span class="badge bg-odysee rounded-pill">Odysee</span>
+              </p>
+              <p v-if="comment.provider == 'youtube'">
+                <span class="badge bg-youtube rounded-pill">Youtube</span>
+              </p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <p class="text-muted me-3">{{ comment.createdDateString }}</p>
+          </div>
         </div>
         <p v-html="comment.text">{{ comment.text.trim() }}</p>
       </div>
