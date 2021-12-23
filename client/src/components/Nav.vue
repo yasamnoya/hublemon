845107f262432551996a50de518ab14d03bcadfd
yasamnoya <yasamnono@gmail.com>
2021-12-23 16:48:46 +0800

    add Navbar

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link class="navbar-brand h1" to="/">Hub 檸檬</router-link>
+    <div class="container">
+      <router-link class="navbar-brand fw-bold fs-3" to="/">Hub 檸檬</router-link>
     </div>
   </nav>
 </template>
